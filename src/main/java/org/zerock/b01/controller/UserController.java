8037305b98f624ece7657469a7f355e5af11fd6a
commang <155868342+ILI-cross@users.noreply.github.com>
2024-05-30 16:50:23 +0900

    login logout 추가

--- src/main/java/org/zerock/b01/controller/UserController.java
+++ src/main/java/org/zerock/b01/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.zerock.b01.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -24,10 +26,10 @@ import java.util.NoSuchElementException;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private HttpSession httpSession;
 
     @GetMapping("/register")
-    public void registerGet(){
-
+    public void registerGet() {
     }
 
     @PostMapping("/register")
@@ -44,17 +46,44 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public void loginGet(){}
+    public String loginGet(HttpServletRequest req) {
+        HttpSession session = req.getSession(false); // 세션이 없으면 새로 생성하지 않고, null을 반환하도록 설정
 
+        if (session != null && session.getAttribute("user") != null) {
+            return "redirect:/board/list"; // 세션에 "user" 속성이 있다면 게시판 목록 페이지로 리디렉션
+        } else {
+            return "/users/login"; // 세션이 없거나 "user" 속성이 없는 경우 로그인 페이지로 이동
+        }
+    }
+
+
+
+
+//    @PostMapping("/login")
+//    public String login(@RequestParam String mid, @RequestParam String mpw, RedirectAttributes redirectAttributes) {
+//        try {
+//            UserDTO userDTO = userService.login(mid, mpw);
+//            httpSession.setAttribute("user", userDTO.getMid());
+//            return "redirect:/board/list"; // 로그인 성공 후 리디렉션
+//        } catch (NoSuchElementException e) {
+//            return "redirect:/users/login";
+//        }
+//    }
     @PostMapping("/login")
-    public String login(@RequestParam String mid, @RequestParam String mpw, RedirectAttributes redirectAttributes) {
+    public String login(@RequestParam String mid, @RequestParam String mpw, RedirectAttributes redirectAttributes, HttpServletRequest req) {
         try {
             UserDTO userDTO = userService.login(mid, mpw);
-            redirectAttributes.addFlashAttribute("message", "User logged in successfully");
+            HttpSession session = req.getSession(); // 세션을 가져옴
+            session.setAttribute("user", userDTO.getMid()); // 로그인 사용자의 아이디를 세션에 저장
             return "redirect:/board/list"; // 로그인 성공 후 리디렉션
         } catch (NoSuchElementException e) {
-            return "redirect:/users/login";
+            return "redirect:/users/login"; // 로그인 실패 시 로그인 페이지로 리디렉션
         }
+    }
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/users/login";
     }
 
 }
